6211772aba6bc70ce0b0d9692628760daebdc4d1
John A <armgardtj@gmail.com>
2017-03-14 16:05:49 -0500

    pls

--- src/Momentum.java
+++ src/Momentum.java
@@ -1,13 +1,5 @@
 
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.image.*;
-import java.io.File;
-import javax.imageio.*;
-import java.io.*;
-
 /**
  * Momentum.java
  *
@@ -22,5 +14,109 @@ public class Momentum {
 
     public Momentum() {
         m1 = m2 = v1 = v2 = null;
+    }
+
+    public Double getM1() {
+        return m1;
+    }
+
+    public Double setM1(String s) {
+        try {
+            m1 = Double.parseDouble(s);
+            return m1;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getM2() {
+        return m2;
+    }
+
+    public Double setM2(String s) {
+        try {
+            m2 = Double.parseDouble(s);
+            return m2;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getV1() {
+        return v1;
+    }
+
+    public Double setV1(String s) {
+        try {
+            v1 = Double.parseDouble(s);
+            return v1;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getV2() {
+        return v2;
+    }
+
+    public Double setV2(String s) {
+        try {
+            v2 = Double.parseDouble(s);
+            return v2;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getP1() {
+        return p1;
+    }
+
+    public Double setP1(String s) {
+        try {
+            p1 = Double.parseDouble(s);
+            return p1;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getP2() {
+        return p2;
+    }
+
+    public Double setP2(String s) {
+        try {
+            p2 = Double.parseDouble(s);
+            return p2;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getCl() {
+        return cl;
+    }
+
+    public Double setCl(String s) {
+        try {
+            cl = Double.parseDouble(s);
+            return cl;
+        } catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+    public Double getCv() {
+        return cv;
+    }
+
+    public Double setCv(String s) {
+        try {
+            cv = Double.parseDouble(s);
+            return cv;
+        } catch (NumberFormatException e){
+            return null;
+        }
     }
 }
